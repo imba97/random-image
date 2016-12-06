466e012e1a4b32169f6f5a4a93b5507a3475982f
imba久期 <mail@imba97.cn>
2016-12-07 03:27:38 +0900

    2016-12-07
    并没什么卵用的东西

--- index.php
+++ index.php
@@ -1,17 +1,35 @@
 <?php
-if(!empty($_GET['update'])&&$_GET['update']==1)
+if(!empty($_POST['animate']))
 {
-  $fileName='randBG.php';     //生成的文件名
-  $imgPath='./boom';          //图片路径（根据这个路径会自动把所有以下设置可用格式的图片读取出来）
-  // $imgPath=empty($_GET['url'])?'./':$_GET['url'];
+  header('Content-type: application/json');
+  $defaultFileName='randBG.php';     //生成的文件名
+  $defaultImgPath='./boom';          //图片路径（根据这个路径会自动把所有以下设置可用格式的图片读取出来）
+  $status=NULL;                  //状态
+  $returnMsg='';
+  $imgPath=empty($_POST['url'])?$defaultImgPath:$_POST['url'];     //如果没有自定义路径则按照以上默认路径
+  $fileName=empty($_POST['name'])?$defaultFileName:$_POST['name']; //如果没有自定义文件名则按照以上默认文件名
   $imgPath=str_replace('\\','/',$imgPath);
   $pathLen=strlen($imgPath);
   $imgPath=$imgPath[$pathLen-1]=='/'?$imgPath:$imgPath.'/';
 
+  $fileName1=explode('.',$fileName);
+  if(is_array($fileName1)&&end($fileName1)!='php') $fileName.='.php';
+  if(empty($fileName1[0]))
+  {
+    $fileName=$defaultFileName;
+    $returnMsg=$returnMsg!=''?$returnMsg:'文件名已改为默认文件名';
+    $status=$status!=NULL?$status:2;
+  }
+
   $imgName=array();
   $reText='';
 
-  $imgDir=scandir($imgPath);
+  $imgDir=@scandir($imgPath);
+  if(!$imgDir)
+  {
+    $returnMsg=$returnMsg!=''?$returnMsg:'没有此文件夹';
+    $status=$status!=NULL?$status:0;
+  }
   $imgNum=count($imgDir);
   for($i=0;$i<$imgNum;$i++)
   {
@@ -27,38 +45,176 @@ if(!empty($_GET['update'])&&$_GET['update']==1)
     }
   }
 
-  $reText.='<?php $images=array(';
-  foreach($imgName as $v)
+  if(count($imgName)==0)
   {
-    $reText.='\''.$v.'\'';
-    if($v!=$imgName[count($imgName)-1])
-    {
-      $reText.=',';
-    }
+    $returnMsg=$returnMsg!=''?$returnMsg:'文件夹中没有图片';
+    $status=$status!=NULL?$status:0;
   }
-  $reText.=');'.'$rand=rand(0,count($images)-1);header("Content-type: image/jpeg");readfile($images[$rand]);?>';
-
-  $file=fopen($fileName,"w+");
-  fwrite($file,$reText);
-  fclose($file);
-  $msg='貌似成功了，一共 '.intval(count($imgName)).' 张图';
-  echo $msg;
+  else
+  {
+    $reText.='<?php $images=array(';
+    foreach($imgName as $v)
+    {
+      $reText.='\''.$v.'\'';
+      if($v!=$imgName[count($imgName)-1])
+      {
+        $reText.=',';
+      }
+    }
+    $reText.=');'.'$rand=rand(0,count($images)-1);header("Content-type: image/jpeg");readfile($images[$rand]);?>';
+    $file=@fopen($fileName,"w+");
+    if(!$file)
+    {
+      $returnMsg=$returnMsg!=''?$returnMsg:'文件创建失败';
+      $status=$status!=NULL?$status:0;
+      $returnInfo=array(
+        'status'    =>  $status,
+        'returnMsg' =>  $returnMsg,
+        'animate'   =>  $_POST['animate']
+      );
+      print_r(json_encode($returnInfo));
+      die();
+    }
+    fwrite($file,$reText);
+    fclose($file);
+    $returnMsg=$returnMsg!=''?$returnMsg:'貌似成功了，一共 '.intval(count($imgName)).' 张图';
+    $status=$status!=NULL?$status:1;
+  }
+  $returnInfo=array(
+    'status'    =>  $status,
+    'returnMsg' =>  $returnMsg,
+    'animate'   =>  $_POST['animate']
+  );
+  print_r(json_encode($returnInfo));
+  die();
 }
-else
-{
 ?>
+
 
 <!DOCTYPE html>
 <head>
 <title>只是一个普通简单并没什么卵用的代码生成工具</title>
+<link rel="stylesheet" type="text/css" href="./style.css" />
 </head>
 <body>
-  <div style="position:relative;margin:100px auto;width:600px;height:200px;background-color:#35a0cf;border-radius:35px;-webkit-box-shadow:3px 3px 3px #CCC;-moz-box-shadow:3px 3px 3px #CCC;box-shadow:3px 3px 3px #CCC;">
-    <a style="position:absolute;top:0;left:0;width:600px;height:200px;line-height:200px;color:#FFF;font-size:40px;font-family:'微软雅黑';text-align:center;text-decoration:none" href='?update=1'>屠龙宝刀点击就Update</a>
-  </div>
+  <div class="abc"></div>
+  <from action="#" method="post">
+    <div class="top-div">
+      <p id="returnMsg"></p>
+      <a class="top-a" id="data-upload" href='javascript:;'>屠龙宝刀点击就Update</a>
+      <a class="btn-data" href="javascript:;">设置</a>
+      <div class="data-div">
+        <div class="data-div-l">
+          <p>指定路径：</p>
+          <p>自定义文件名：</p>
+        </div>
+        <div class="data-div-r">
+          <p><input type="text" id="getUrl" /></p>
+          <p><input type="text" id="getName" /></p>
+        </div>
+        <div class="data-div-b"></div>
+        <a class="data-div-confirm" href="javascript:;">确定</a>
+      </div>
+    </div>
+  </from>
 </body>
-</html>
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+<script type="text/javascript" language="javascript">
+var checkAnimate=true;
+$(document).ready(function(){
+  $(".btn-data,.data-div-confirm").click(function(){
+    $(this).blur();
+    if(!checkAnimate) return;
+    checkAnimate=false;
+    if($(".data-div").is(':hidden'))
+    {
+      $(".data-div").css({'display':'block','opacity':0}).animate({'left':0,'opacity':1},200,function(){
+        checkAnimate=true;
+        $(".btn-data").html('取消');
+      });
+    }
+    else
+    {
+      $(".data-div").animate({'left':-100,'opacity':0},200,function(){
+        checkAnimate=true;
+        $(this).css({'display':'none'});
+        $(".btn-data").html('设置');
+      });
 
-<?php
+      //确认按钮被点击
+      var btn=$(this).attr('class');
+      if(btn=='data-div-confirm')
+      {
+        var url=$("#getUrl").val();
+        var name=$("#getName").val();
+        var data={
+          'url':url,
+          'name':name,
+          'animate':true
+        };
+        action(data);
+      }
+    }
+  });
+
+  $("#data-upload").click(function(){
+    action({'url':'./taidada','name':'randBG.php','animate':false});
+  });
+
+  //键盘事件，回车倍按下后等于点击确认按钮
+  $(document).keypress(function(e){
+    if(e.keyCode==13)
+    {
+      if(!$(".data-div").is(":hidden"))
+      {
+        $(".data-div-confirm").click();
+      }
+    }
+  });
+});
+
+function action(data,animate=false)
+{
+  $.ajax({
+    url:'./index.php',
+    async:false,
+    data:data,
+    type:'POST',
+    dataType:'json',
+    success:function(json){
+      $("#returnMsg").html(json.returnMsg);
+      switch(json.status)
+      {
+        case 0:
+          $("#returnMsg").css('background-color','#852e2e');
+        break;
+        case 1:
+          $("#returnMsg").css('background-color','#4c852e');
+        break;
+        case 2:
+          $("#returnMsg").css('background-color','#969117');
+        break;
+      }
+      if(json.animate)
+      {
+        setTimeout(function(){
+          checkAnimate=false;
+          $("#data-upload").animate({'left':-100,'opacity':0},200,function(){
+            $(this).css({'display':'none'});
+            checkAnimate=true;
+          });
+        },200);
+      }
+      else
+      {
+        checkAnimate=false;
+        $("#data-upload").animate({'left':-100,'opacity':0},200,function(){
+          $(this).css({'display':'none'});
+          checkAnimate=true;
+        });
+      }
+    }
+  });
 }
-?>
+</script>
+</html>
